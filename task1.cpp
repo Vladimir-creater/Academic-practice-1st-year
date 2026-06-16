f2d13bdd455ce21c2ebe64bb5783d1e91fc8cd49
Vladimir-creater <buhaev.voloday@gmail.com>
2026-06-16 19:57:14 +0800

    Добавил функцию восстановления пути, исправил вывод минимального пути согласно ТЗ

--- task1.cpp
+++ task1.cpp
@@ -16,6 +16,9 @@ int main() {
       std::cin >> pyramid[i][j];
     }
   }
+
+  // Создаём копию пирамиды, чтобы восстановить путь
+  std::vector<std::vector<int>> original_pyramid = pyramid;
   
   // Алгоритм вывода минимальной суммы на позицию pyramid[0][0]
   for (int i = N - 2; i >= 0; i--) {
@@ -23,9 +26,30 @@ int main() {
       pyramid[i][j] += std::min(pyramid[i + 1][j], pyramid[i + 1][j + 1]);
     }
   }
+
+  // Восстановление пути
+  std::vector<int> route; // Место хранения пути
+  int j = 0;
+
+  for (int i = 0; i < N - 1; i++) {
+    route.push_back(original_pyramid[i][j]);
+    if (pyramid[i + 1][j] <= pyramid[i + 1][j + 1]) {
+      // Если левое меньше, индекс j не меняется
+    } else {
+      // Если правое меньше, индекс j меняется на j + 1
+      j++;
+    }
+  }
+  // Не забываем последнее число
+  route.push_back(original_pyramid[N - 1][j]);
   
   // Вывод минимальной суммы
-  std::cout << "Minimum amount: " << pyramid[0][0] << '\n';
+  std::cout << pyramid[0][0] << '\n';
+
+  //Вывод маршрута через пробел
+  for (int i = 0; i < N; i++) {
+    std::cout << route[i] << " ";
+  }
 
   return 0;
 }
